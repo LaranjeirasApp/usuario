61360aa861d2c13327a51893a0108e5222a25da2
LaranjeirasApp <quadrascontrole@gmail.com>
2026-07-04 18:29:55 -0300

    Add files via upload

---
+++ firebase-config-usuario.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBKDqCKiV2OzqDUMYTU1BJLNG9oFAGpMeU",
+  authDomain: "controlequadras-95e02.firebaseapp.com",
+  databaseURL: "https://controlequadras-95e02-default-rtdb.firebaseio.com",
+  projectId: "controlequadras-95e02",
+  storageBucket: "controlequadras-95e02.firebasestorage.app",
+  messagingSenderId: "548582626548",
+  appId: "1:548582626548:web:0858affd903b39c6743aa1"
+};
